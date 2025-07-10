6435736a4f6abdfc82571fcf1ec1acefa6b44ef6
Ailton_78 <sd.ailtonsampaio@gmail.com>
2025-07-10 19:59:35 -0300

    Referenciando os post do usuario

--- src/main/java/com/ailton78/worckshopmongo/domain/User.java
+++ src/main/java/com/ailton78/worckshopmongo/domain/User.java
@@ -1,22 +1,29 @@
 package com.ailton78.worckshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-@Document(collection="user")
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
-	public User() {}
-	
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList();
+
+	public User() {
+	}
+
 	public User(String id, String name, String email) {
 		this.id = id;
 		this.name = name;
@@ -46,7 +53,30 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
