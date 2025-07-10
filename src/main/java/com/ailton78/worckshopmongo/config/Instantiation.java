c6efa2ae47a13108a1aab9ed1744ae75641cdf67
Ailton_78 <sd.ailtonsampaio@gmail.com>
2025-07-10 19:37:36 -0300

    Projeção dos dados do Author

--- src/main/java/com/ailton78/worckshopmongo/config/Instantiation.java
+++ src/main/java/com/ailton78/worckshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ailton78.worckshopmongo.domain.Post;
 import com.ailton78.worckshopmongo.domain.User;
+import com.ailton78.worckshopmongo.dto.AuthorDTO;
 import com.ailton78.worckshopmongo.repository.PostRepository;
 import com.ailton78.worckshopmongo.repository.UserRepository;
 
@@ -28,16 +29,20 @@ public class Instantiation implements CommandLineRunner {
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem",
-				"Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", maria);
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!",
+				new AuthorDTO(maria));
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
