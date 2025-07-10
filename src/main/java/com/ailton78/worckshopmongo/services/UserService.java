c51268be33fe7ad5df2a3dda1553b1119210d484
Ailton_78 <sd.ailtonsampaio@gmail.com>
2025-07-10 11:31:02 -0300

    Obtendo usuario por id

--- src/main/java/com/ailton78/worckshopmongo/services/UserService.java
+++ src/main/java/com/ailton78/worckshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ailton78.worckshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ailton78.worckshopmongo.domain.User;
 import com.ailton78.worckshopmongo.repository.UserRepository;
+import com.ailton78.worckshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User>findAll(){
 		  return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
