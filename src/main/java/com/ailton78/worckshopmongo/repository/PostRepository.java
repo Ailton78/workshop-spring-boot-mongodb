3cc37eeda16bcbb6445996ce22b1fc82b1a0daae
Ailton_78 <sd.ailtonsampaio@gmail.com>
2025-07-11 11:27:46 -0300

    Consulta simples com query methods

--- src/main/java/com/ailton78/worckshopmongo/repository/PostRepository.java
+++ src/main/java/com/ailton78/worckshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.ailton78.worckshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ailton78.worckshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post>findByTitleContainingIgnoreCase(String text);
 
 }
